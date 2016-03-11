137c29304735966df2c2efd580c6f803d9270498
nassir <nassirmz@gmail.com>
2016-03-11 15:10:44 -0800

    Add boardInfo and scoreBoard objects

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,20 @@
-// start slingin' some d3 here.
+// one object holds info on svg width, heigh ... DONE
+// one object hold scoreboard info, maxScore, curScore DONE
+// create class called asteroid with properties size, widith, color ...
+  // add methods
+    // calculate where to move ...
+    // change size every 300 ms
+    // do something if collision
+// create class called player with properties like size width color, bind to arrow keys
+  // methods
+    // check for collisions ????
+
+var boardInfo = {
+  width: 600,
+  height: 400
+};
+
+var scoreBoard = {
+  curScore: 0,
+  maxScore: 0
+}
