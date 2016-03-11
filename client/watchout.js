c26cd2a3e7601fd446ef3ad2b10bd5c2525876e3
nassir <nassirmz@gmail.com>
2016-03-11 15:53:41 -0800

    Add 20 randomly generator asteroids to the board

--- client/watchout.js
+++ client/watchout.js
@@ -21,7 +21,7 @@ var scoreBoard = {
 }
 
 var Asteroid = function() {
-  this.size = Math.random() * 30 + 20;
+  this.size = Math.random() * 15 + 10;
   this.x = Math.random() * boardInfo.width;
   this.y = Math.random() * boardInfo.height;
 }
@@ -32,8 +32,20 @@ var Player = function() {
   this.y = 200;
 }
 
-d3.select(".board").append("svg")
+var svg = d3.select(".board").append("svg")
   .attr("width", boardInfo.width)
   .attr("height", boardInfo.height);
+
+var asteroids = [];
+
+for(var i = 0; i<20; i+=1) {
+  asteroids.push(new Asteroid());
+}
+
+svg.selectAll(".asteroid").data(asteroids)
+  .enter().append("circle").attr("r", function(d) {return d.size})
+  .attr("cx", function(d) {return d.x;})
+  .attr("cy", function(d) { return d.y;});
+
 
 
